f14674207e782c80e28d5a9d3aff042e3093e2af
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-28 20:24:02 +0200

    installing entity framework and other tool

--- SmartCity-API/Program.cs
+++ SmartCity-API/Program.cs
@@ -1,3 +1,4 @@
+using Infrastructure.DB;
 
 namespace SmartCity_API
 {
@@ -12,8 +13,10 @@ namespace SmartCity_API
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
+       
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
