83e6e7208a2abadda6173814236368ae5734e8dc
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-28 19:48:41 +0200

    completing Place mode

---
+++ Domain/Entities/Place.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Place
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public string Address { get; set; } = string.Empty;
+        public string? City { get; set; }
+
+        //foreinKeys
+
+        public int categoryId { get; set; }
+
+        public Category Category { get; set; }
+
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<PlaceImage> Images { get; set; } = new List<PlaceImage>();
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+
+    }
+}
