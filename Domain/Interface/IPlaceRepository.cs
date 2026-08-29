fccef61c65ef3f46f07438a5b423dd94fa07d984
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-29 23:04:56 +0200

    adding place interface

---
+++ Domain/Interface/IPlaceRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface
+{
+    public interface IPlaceRepository
+    {
+        Task<IEnumerable<Place>> GetPlacesAsync();
+
+        Task<Place?> GetPlaceById(int id);
+
+        Task<IEnumerable<Place>> GetNearbyAsync(double latitude, double longitude, double radiusKm, int? categoryId = null);
+
+        Task AddAsync(Place place);
+        Task UpdateAsync(Place place);
+
+        Task DeleteAsync(int id);
+
+
+
+    }
+}
