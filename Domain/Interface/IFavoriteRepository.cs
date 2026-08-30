7ac4166525d79a0610a0c40dd5e3153e383ad4af
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-30 23:45:55 +0200

    adding Favorite repository

---
+++ Domain/Interface/IFavoriteRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface
+{
+    public interface IFavoriteRepository
+    {
+        Task<IEnumerable<Favorite>> GetByUserIdAsync(string userId);
+        Task<bool> ExistsAsync(string userId, int placeId);
+        Task<Favorite> AddAsync(Favorite favorite);
+        Task DeleteAsync(int userId, int placeId);
+    }
+}
