ee701c920359bdc134651efd7691833ef46091f4
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-30 23:21:19 +0200

    adding category interface

---
+++ Domain/Interface/ICategory.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface
+{
+    public interface ICategory
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task<Category> AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
