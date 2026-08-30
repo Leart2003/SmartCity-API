94a207bea084626b92304cd44235d07ad7346d3d
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-30 23:22:13 +0200

    adding IPlaceImageRepository interface

---
+++ Domain/Interface/IPlaceImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface
+{
+    public interface IPlaceImageRepository
+    {
+        Task<IEnumerable<PlaceImage>> GetByPlaceIdAsync(int placeId);
+        Task<PlaceImage?> GetByIdAsync(int id);
+        Task<PlaceImage> AddAsync(PlaceImage image);
+        Task DeleteAsync(int id);
+    }
+}
