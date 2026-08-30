f21b747d36dae58c49ec2a134aa8a901070f10ee
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-30 23:45:08 +0200

    adding review interface

---
+++ Domain/Interface/IReviewRepository'.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface
+{
+    public interface IReviewRepository_
+    {
+
+        Task<IEnumerable<Review>> GetByPlaceIdAsync(int placeId);
+        Task<Review?> GetByIdAsync(int id);
+        Task<double> GetAverageRatingAsync(int placeId);
+        Task<Review> AddAsync(Review review);
+        Task UpdateAsync(Review review);
+        Task DeleteAsync(int id);
+    }
+}
