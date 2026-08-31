095bac60e55958fb32830dd5554765b3d4b99699
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-31 02:06:36 +0200

    Implementing place interface and creating GetPlacesAsync

---
+++ Infrastructure/Repository/PlaceRepository.cs
@@ -0,0 +1,53 @@
+﻿using Domain.Entities;
+using Domain.Interface;
+using Infrastructure.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class PlaceRepository : IPlaceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlaceRepository(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public Task AddAsync(Place place)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Place>> GetNearbyAsync(double latitude, double longitude, double radiusKm, int? categoryId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Place?> GetPlaceById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Place>> GetPlacesAsync()
+        {
+            return await _context.Places
+                .Include(p => p.Category)
+                .Include(p => p.Images)
+                .ToListAsync();
+        }
+
+        public Task UpdateAsync(Place place)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
