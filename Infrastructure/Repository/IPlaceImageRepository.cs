3dd7d0483ec95a35f7717de020eccfbf85866cec
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-01 23:10:27 +0200

    Adding Place Repository

---
+++ Infrastructure/Repository/IPlaceImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class PlaceImageRepository : IPlaceImageRepository
+    {
+
+    }
+}
