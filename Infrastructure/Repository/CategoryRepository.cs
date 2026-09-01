e3da05ad1fbb44be08ea4f0aff7eec0bed94ab4b
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-01 23:07:18 +0200

    adding Category Interface

---
+++ Infrastructure/Repository/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class CategoryRepository : ICategory
+    {
+        public Task<Category> AddAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
