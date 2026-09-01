60302ea8f0fc6da43828a03476186d0df558d502
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-01 23:19:01 +0200

    adding Review Repository

---
+++ Infrastructure/Repository/ReviewRepistory.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class ReviewRepistory : IReviewRepository_
+
+    {
+        public Task<Review> AddAsync(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<double> GetAverageRatingAsync(int placeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Review>> GetByPlaceIdAsync(int placeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Review review)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
