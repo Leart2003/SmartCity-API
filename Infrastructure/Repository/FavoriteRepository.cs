0f02b09028fdaace6be8583c799885d2f605e974
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-02 22:49:01 +0200

    finishing favorite repository

--- Infrastructure/Repository/FavoriteRepository.cs
+++ Infrastructure/Repository/FavoriteRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Domain.Entities;
 using Domain.Interface;
+using Infrastructure.DB;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,24 +10,43 @@ namespace Infrastructure.Repository
 {
     public class FavoriteRepository : IFavoriteRepository
     {
-        public Task<Favorite> AddAsync(Favorite favorite)
+        private readonly ApplicationDbContext _context;
+        public FavoriteRepository(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task<Favorite> AddAsync(Favorite favorite)
+        {
+            _context.AddAsync(favorite);
+            await _context.SaveChangesAsync();
+            return favorite;
         }
 
-        public Task DeleteAsync(string userId, int placeId)
+        public async Task DeleteAsync(string userId, int placeId)
         {
-            throw new NotImplementedException();
+            var favorite = await _context.Favorites
+                .FirstOrDefaultAsync(f => f.UserId == userId && f.PlaceId == placeId);
+
+            if (favorite != null)
+            {
+                _context.Favorites.Remove(favorite);
+                await _context.SaveChangesAsync();
+            }
         }
 
-        public Task<bool> ExistsAsync(string userId, int placeId)
+        public async Task<bool> ExistsAsync(string userId, int placeId)
         {
-            throw new NotImplementedException();
+            return await _context.Favorites
+                .AnyAsync(f => f.UserId == userId && f.PlaceId == placeId);
         }
 
-        public Task<IEnumerable<Favorite>> GetByUserIdAsync(string userId)
+        public async Task<IEnumerable<Favorite>> GetByUserIdAsync(string userId)
         {
-            throw new NotImplementedException();
+         return await _context.Favorites
+                .Include(f => f.Place)
+                .ThenInclude(p => p.Category)
+                .Where(f => f.UserId == userId)
+                .ToListAsync();
         }
     }
 }
