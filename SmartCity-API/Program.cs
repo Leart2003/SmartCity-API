8f19618a8a073c44a5e75d63e965416d30823a47
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-04 23:44:37 +0200

    fixing errors

--- SmartCity-API/Program.cs
+++ SmartCity-API/Program.cs
@@ -1,9 +1,11 @@
 using Domain.Entities;
 using Domain.Interface;
+using Domain.Mappers;
 using Infrastructure.DB;
 using Infrastructure.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SmartCity_API.MappingProfiles;
 
 namespace SmartCity_API
 {
@@ -13,11 +15,9 @@ namespace SmartCity_API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
             {
@@ -27,24 +27,29 @@ namespace SmartCity_API
             })
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
+
+            builder.Services.AddScoped<IPlaceRepository, PlaceRepository>();
             builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
             builder.Services.AddScoped<IPlaceImageRepository, PlaceImageRepository>();
             builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
             builder.Services.AddScoped<IFavoriteRepository, FavoriteRepository>();
-            var app = builder.Build();
-       
 
-            // Configure the HTTP request pipeline.
+            // Mappers
+            builder.Services.AddAutoMapper(cfg =>
+            {
+                cfg.AddProfile<PlaceProfile>();
+                cfg.AddProfile<CategoryProfile>();
+            });
+
+            var app = builder.Build();
+
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
