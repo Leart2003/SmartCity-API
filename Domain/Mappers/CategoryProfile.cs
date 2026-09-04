eb0c9cdfda7e5fe3faefafa7586417c190292028
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-04 23:39:40 +0200

    adding category mapper

---
+++ Domain/Mappers/CategoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Mappers
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CreateCategoryDto, Category>();
+        }
+    }
+}
