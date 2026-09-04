3530cbc5e6db00c0f5be74162d76e392e2fc7235
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-04 23:38:11 +0200

    adding CategoryDto

---
+++ Domain/Dtos/CategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? IconName { get; set; }
+    }
+}
