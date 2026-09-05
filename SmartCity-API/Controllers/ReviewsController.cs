626725c257b14705146072815b74a7370408b84b
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-05 23:51:36 +0200

    adding place by id method

---
+++ SmartCity-API/Controllers/ReviewsController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartCity_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewsController(IReviewRepository reviewRepository, IMapper mapper)
+        {
+            _reviewRepository = reviewRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("place/{placeId}")]
+        public async Task<ActionResult<IEnumerable<ReviewDto>>> GetByPlaceId(int placeId)
+        {
+            var reviews = await _reviewRepository.GetByPlaceIdAsync(placeId);
+            return Ok(_mapper.Map<IEnumerable<ReviewDto>>(reviews));
+        }
+
+    }
+}
