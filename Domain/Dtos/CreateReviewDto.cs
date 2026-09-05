a74bda4b91a753f908b9a02df85ed5bf8580156e
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-05 23:39:22 +0200

    adding reviewDto, and CreateReviewDto

---
+++ Domain/Dtos/CreateReviewDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dtos
+{
+    public class CreateReviewDto
+    {
+        public int PlaceId { get; set; }
+        public int Rating { get; set; } 
+        public string? Comment { get; set; }
+    }
+}
---
+++ Domain/Dtos/ReviewDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dtos
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public int PlaceId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string UserFullName { get; set; } = string.Empty;
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
