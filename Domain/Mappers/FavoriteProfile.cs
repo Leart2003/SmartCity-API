89840e941eb3adbf8ee74a238ef08a83c576d2f4
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-07 23:37:59 +0200

    adding Favorite Mapper

---
+++ Domain/Mappers/FavoriteProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Mappers
+{
+    public class FavoriteProfile : Profile
+    {
+        public FavoriteProfile()
+        {
+            CreateMap<Favorite, FavoriteDto>()
+               .ForMember(dest => dest.PlaceName, opt => opt.MapFrom(src => src.Place.Name))
+               .ForMember(dest => dest.CoverImageUrl, opt => opt.MapFrom(src =>
+                   src.Place.Images.Any(i => i.IsCoverImage)
+                       ? src.Place.Images.First(i => i.IsCoverImage).ImageUrl
+                       : src.Place.Images.FirstOrDefault() != null ? src.Place.Images.First().ImageUrl : null));
+        }
+    }
+}
