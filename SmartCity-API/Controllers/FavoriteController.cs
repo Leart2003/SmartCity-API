361db4de47f359797a07016cb24d0422edb96e67
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-07 23:39:06 +0200

    Injecting services

---
+++ SmartCity-API/Controllers/FavoriteController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Domain.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartCity_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FavoriteController : ControllerBase
+    {
+        private readonly IFavoriteRepository _favoriteRepository;
+        private readonly IMapper _mapper;
+
+        public FavoriteController(IFavoriteRepository favoriteRepository, IMapper mapper)
+        {
+            _favoriteRepository = favoriteRepository;
+            _mapper = mapper;
+        }
+
+
+    }
+}
