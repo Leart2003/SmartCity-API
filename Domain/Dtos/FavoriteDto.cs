335901a632abb9347b2b9f1478c016b98320268f
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-07 23:36:56 +0200

    adding FavoriteDto

---
+++ Domain/Dtos/FavoriteDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dtos
+{
+    public class FavoriteDto
+    {
+        public int Id { get; set; }
+        public int PlaceId { get; set; }
+        public string PlaceName { get; set; } = string.Empty;
+        public string? CoverImageUrl { get; set; }
+        public DateTime AddedAt { get; set; }
+    }
+}
