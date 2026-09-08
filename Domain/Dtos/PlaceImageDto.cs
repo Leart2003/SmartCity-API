78af0f389665a7acd70cbc02c2e3b3da8620df78
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-08 23:52:02 +0200

    adding PlaceImageDto

---
+++ Domain/Dtos/PlaceImageDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dtos
+{
+    public class PlaceImageDto
+    {
+        public int Id { get; set; }
+        public int PlaceId { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsCoverImage { get; set; }
+    }
+}
