d98f3213724d2c36a8e242f7fe927b329f6c46ce
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-09 23:34:51 +0200

    Adding GetByPlaceId in PlaceImage controller

---
+++ Domain/Mappers/PlaceImageProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Mappers
+{
+    public class PlaceImageProfile : Profile
+    {
+        public PlaceImageProfile()
+        {
+            CreateMap<PlaceImage, PlaceImageDto>();
+            CreateMap<CreatePlaceImageDto, PlaceImage>();
+        }
+
+    }
+}
---
+++ SmartCity-API/Controllers/PlaceImageController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartCity_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaceImageController : ControllerBase
+    {
+
+        private readonly IPlaceImageRepository _placeImageRepository;
+        private readonly IMapper _mapper;
+
+        public PlaceImageController(IPlaceImageRepository placeImageRepository, IMapper mapper)
+        {
+            _placeImageRepository = placeImageRepository;
+            _mapper = mapper;
+        }
+        [HttpGet("place/{placeId}")]
+        public async Task<ActionResult<IEnumerable<PlaceImageDto>>> GetByPlaceId(int placeId)
+        {
+            var images = await _placeImageRepository.GetByPlaceIdAsync(placeId);
+            return Ok(_mapper.Map<IEnumerable<PlaceImageDto>>(images));
+        }
+    }
+}
