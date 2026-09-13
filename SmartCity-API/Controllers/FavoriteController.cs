43dd0b28bc3769e7bed359c2ee87b3237595ff36
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-13 23:51:42 +0200

    documenting FavoriteController

--- SmartCity-API/Controllers/FavoriteController.cs
+++ SmartCity-API/Controllers/FavoriteController.cs
@@ -9,6 +9,9 @@ using System.Security.Claims;
 
 namespace SmartCity_API.Controllers
 {
+    /// <summary>
+    /// Provides endpoints for managing the authenticated user's favorite places.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
@@ -17,13 +20,34 @@ namespace SmartCity_API.Controllers
         private readonly IFavoriteRepository _favoriteRepository;
         private readonly IMapper _mapper;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FavoriteController"/> class.
+        /// </summary>
+        /// <param name="favoriteRepository">
+        /// Repository used to access and manage favorite places.
+        /// </param>
+        /// <param name="mapper">
+        /// AutoMapper instance used to map entities to DTOs.
+        /// </param>
         public FavoriteController(IFavoriteRepository favoriteRepository, IMapper mapper)
         {
             _favoriteRepository = favoriteRepository;
             _mapper = mapper;
         }
 
-       
+        /// <summary>
+        /// Retrieves all favorite places belonging to the currently authenticated user.
+        /// </summary>
+        /// <returns>
+        /// A collection of <see cref="FavoriteDto"/> objects representing the user's favorites.
+        /// </returns>
+        /// <response code="200">
+        /// The user's favorite places were successfully retrieved.
+        /// </response>
+        /// <response code="401">
+        /// The user is not authenticated or their user ID could not be determined.
+        /// </response>
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<FavoriteDto>>> GetMyFavorites()
         {
@@ -34,6 +58,22 @@ namespace SmartCity_API.Controllers
             var favorites = await _favoriteRepository.GetByUserIdAsync(userId);
             return Ok(_mapper.Map<IEnumerable<FavoriteDto>>(favorites));
         }
+        /// <summary>
+        /// Adds a place to the currently authenticated user's favorites.
+        /// </summary>
+        /// <param name="placeId">The unique identifier of the place to add.</param>
+        /// <returns>
+        /// The newly created <see cref="FavoriteDto"/>.
+        /// </returns>
+        /// <response code="200">
+        /// The place was successfully added to the user's favorites.
+        /// </response>
+        /// <response code="400">
+        /// The place is already in the user's favorites.
+        /// </response>
+        /// <response code="401">
+        /// The user is not authenticated or their user ID could not be determined.
+        /// </response>
         [HttpPost("{placeId}")]
         public async Task<ActionResult<FavoriteDto>> Add(int placeId)
         {
@@ -53,7 +93,22 @@ namespace SmartCity_API.Controllers
 
             var created = await _favoriteRepository.AddAsync(favorite);
             return Ok(_mapper.Map<FavoriteDto>(created));
-        }
+        }    /// <summary>
+             /// Removes a place from the currently authenticated user's favorites.
+             /// </summary>
+             /// <param name="placeId">The unique identifier of the place to remove.</param>
+             /// <returns>
+             /// No content if the place was successfully removed.
+             /// </returns>
+             /// <response code="204">
+             /// The place was successfully removed from the user's favorites.
+             /// </response>
+             /// <response code="401">
+             /// The user is not authenticated or their user ID could not be determined.
+             /// </response>
+             /// <response code="404">
+             /// The place does not exist in the user's favorites.
+             /// </response>
 
         [HttpDelete("{placeId}")]
         public async Task<IActionResult> Remove(int placeId)
