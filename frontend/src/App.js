b2b89321f539d3a0a63e47e4963e917965ea62c9
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-09-22 23:22:20 +0200

    cofiguring routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,46 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import PlaceDetailsPage from "./pages/PlaceDetailsPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import ProtectedRoute from "./components/common/ProtectedRoute";
+
 function App() {
   return (
-    <main>
-      <h1>SmartCity</h1>
-    </main>
+<div className="d-flex flex-column min-vh-100">
+   <Navbar/>
+  <main className="flex-grow-1">
+    <Routes>
+       <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/places/:id" element={<PlaceDetailsPage />} />
+
+          <Route
+            path="/favorites"
+            element={
+              <ProtectedRoute>
+                <FavoritesPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute requireAdmin={true}>
+                <AdminDashboardPage />
+              </ProtectedRoute>
+            }
+          />
+    </Routes>
+  </main>
+    <Footer />
+</div>
   );
 }
 
--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -6,7 +6,7 @@ const AuthContext = createContext(null);
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
-  // Kur aplikacioni niset, kontrollo nëse ka user të ruajtur në localStorage
+  
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
 
